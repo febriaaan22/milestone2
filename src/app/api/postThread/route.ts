3421a6aecb7f15481d9563fae9f53bdbe2a51aaf
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2024-03-29 00:11:25 +0700

    Delete route

--- src/app/api/postThread/route.ts
+++
@@ -1,6 +0,0 @@
-import { PostThread } from "@/server-action/controllers/PostThread.controller";
-import { NextResponse, NextRequest } from "next/server";
-export async function POST(req: NextRequest, res: NextResponse) {
-    return await PostThread(req, res);
-  }
-  
