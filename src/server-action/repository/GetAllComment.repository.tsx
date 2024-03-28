00545d34e0a8e7357769f840cfbd80e3383eba29
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2024-03-29 01:47:47 +0700

    update: Change Respond Get All Comment on GetAllComment repository

--- src/server-action/repository/GetAllComment.repository.tsx
+++ src/server-action/repository/GetAllComment.repository.tsx
@@ -1,14 +1,29 @@
+// repository/GetAllComment.repository.js
+
 import Comments from "@/models/Comments.model";
+import Threads from "@/models/Threads.model";
+import Users from "@/models/Users.model";
 import { GetAllCommentbyThreadId } from "../types/Comment.type";
+
 export const GetAllComment = async ({ threadId }: GetAllCommentbyThreadId) => {
-    try{
-      const getAllCommentbyThreadid = await Comments.findAll({
-        where: {
-          threads_id: threadId,
+  try {
+    const getAllCommentbyThreadid = await Comments.findAll({
+      where: {
+        threads_id: threadId,
+      },
+      include: [
+        {
+          model: Threads,
+          attributes: ["thread_title"],
         },
-      });
-      return getAllCommentbyThreadid
-  } catch(err){
+        {
+          model: Users,
+          attributes: ["user_name"],
+        },
+      ],
+    });
+    return getAllCommentbyThreadid;
+  } catch (err) {
     console.error("Error get all comment:", err);
     throw err;
   }
