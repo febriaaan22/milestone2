a89bd50e7abf222af9aa2bc3c27741b4456176a4
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2024-03-28 21:34:31 +0700

    update: DbConnection

--- src/server-action/middlewares/dbConnection.ts
+++ src/server-action/middlewares/dbConnection.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
-const DB_URL = "postgres://postgres.jptzyotwsjhmphakryap:Genierevounext@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
+const DB_URL = "postgres://postgres.cgutdazbgtxmfngivgnm:Serenrevounext!@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres";
+
 
 const sequelize = new Sequelize(DB_URL, {
     dialect: "postgres",
