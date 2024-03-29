b0fe37532bef552ddfd22930d405532fa7c57ca4
Sigit Wiranto <wirsigit@gmail.com>
2024-03-29 19:08:02 +0700

    SS-11 feat: add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { CTAForumCard, Navbar } from "@/components";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       {/* mounted for testing purposes only */}
-      <div style={{ height: "90vh", display: "flex", alignItems: "center" }}>
+      <div style={{ height: "90vh" }}>
         <CTAForumCard />
+        <Footer />
       </div>
     </>
   );
