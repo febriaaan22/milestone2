4c3ec5c68c84c059f913691aa4aa5f5899ae1f13
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2024-03-31 22:53:29 +0700

    Create Get API "All Threads"

---
+++ src/server-action/controllers/GetThreads.controller.tsx
@@ -0,0 +1,24 @@
+import { NextResponse, NextRequest } from "next/server";
+import { GetAllThreads } from "../services/Threads.service";
+
+export const GetThreads = async (req: NextRequest, res: NextResponse) => {
+	try {
+		const getAllThread = await GetAllThreads();
+		return NextResponse.json(
+			{
+				success: true,
+				message: "Success get all Threads",
+				data: getAllThread,
+			},
+			{ status: 200 }
+		);
+	} catch (error: any) {
+		return NextResponse.json(
+			{
+				success: false,
+				message: error.message,
+			},
+			{ status: 400 }
+		);
+	}
+};
