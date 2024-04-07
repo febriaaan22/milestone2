1a2369cabd90d2bf331c9e5a59306dc9a92ba2eb
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2024-04-07 22:31:42 +0700

    Revise Category_id

--- src/server-action/repository/PostThreads.repository.tsx
+++ src/server-action/repository/PostThreads.repository.tsx
@@ -3,30 +3,30 @@ import Categories from "@/models/Categories.model";
 import { PostThreads as inputThreads } from "../types/threads.type";
 
 export const PostThread = async ({
-  threadTitle,
-  category,
-  userId,
-  threadDesc,
+	threadTitle,
+	category,
+	userId,
+	threadDesc,
 }: inputThreads) => {
-  try {
-    const findCategoryId = await Categories.findOne({
-        where:{
-            category_name: category
-        },
-        attributes: ["category_id"]
-    })
-    const categoryId = findCategoryId?.getDataValue("category_id") as number;
-    const threadDate = new Date();
-    const postingThread = await Threads.create({
-      thread_title: threadTitle,
-      category_id: categoryId,
-      user_id: userId,
-      thread_date: threadDate,
-      thread_desc: threadDesc,
-    });
-    return postingThread;
-  } catch (err) {
-    console.error("Error posting Thread:", err);
-    throw err;
-  }
+	try {
+		const findCategoryId = await Categories.findOne({
+			where: {
+				category_id: category,
+			},
+			attributes: ["category_id"],
+		});
+		const categoryId = findCategoryId?.getDataValue("category_id") as number;
+		const threadDate = new Date();
+		const postingThread = await Threads.create({
+			thread_title: threadTitle,
+			category_id: categoryId,
+			user_id: userId,
+			thread_date: threadDate,
+			thread_desc: threadDesc,
+		});
+		return postingThread;
+	} catch (err) {
+		console.error("Error posting Thread:", err);
+		throw err;
+	}
 };
