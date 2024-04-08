74a78d93801911ea95ebd910c4709709ecb23123
Dody Hartanto <muharridody@gmail.com>
2024-04-08 21:15:44 +0800

    SS-14 feat: add mental health menue page

---
+++ src/app/(routes)/mental-problems/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "@/components";
+import { MentalProblemsContainer } from "@/containers";
+
+const MentalProblemsPage = () => {
+  return (
+    <>
+      <Navbar />
+      <MentalProblemsContainer />;
+    </>
+  );
+};
+
+export default MentalProblemsPage;
