9b059f133894d12ebb220469e8399f435140dba7
Resyana Cahyanita <resyanac@gmail.com>
2024-04-09 10:36:32 +0700

    feat : modal discussion component

---
+++ src/components/ModalDiscussion/index.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { Modal, Typography, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import axios from "axios";
+import PrimaryButton from "../button/primaryButton";
+import SecondaryButton from "../button/secondaryButton";
+import styles from "./ModalDiscussion.module.scss";
+
+const ModalDiscussion: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete("api-endpoint");
+      handleClose();
+    } catch (error) {
+      console.error("Error deleting:", error);
+      throw error;
+    }
+  };
+
+  const handleDeleteClick = () => {
+    handleDelete().catch((error) => {
+      console.error("Error:", error);
+    });
+  };
+
+  return (
+    <div>
+      <div className={styles.ModalDiscussion}>
+        <PrimaryButton text="Start Discussion" onClick={handleOpen} />
+      </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+        <div className={styles.modalWrapper}>
+          <div className={styles.modalContent}>
+            <IconButton className={styles.closeButton} onClick={handleClose}>
+              <CloseIcon />
+            </IconButton>
+            <img
+              src="/icons/discuss.png"
+              alt="Modal"
+              className={styles.modalImage}
+            />
+            <Typography variant="h2" gutterBottom style={{ fontSize: "38px" }}>
+              Oops, one more step!
+            </Typography>
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+              Please Register or Login to start discussion
+            </Typography>
+            <div className={styles.modalButtonGroup}>
+              <SecondaryButton text="Register" onClick={handleClose} />
+              <div style={{ width: "16px" }}></div>
+              <PrimaryButton
+                text="Login"
+                onClick={handleDeleteClick}
+                isLoading={false}
+              />
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalDiscussion;
