b1ab3e225a79c2f315b861d856aa5acd46ba29db
Dody Hartanto <muharridody@gmail.com>
2024-04-17 22:00:05 +0800

    fix: fix import component for deployment

--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -1,44 +1,20 @@
 import React from "react";
-import { usePathname } from "next/navigation";
-import { Breadcrumbs as MyBreadcrumbs, useTheme } from "@mui/material/";
-import Link from "@mui/material/Link";
-import Typography from "@mui/material/Typography";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { Box, Breadcrumbs, Link } from "@mui/material";
+import { NavigateNext } from "@mui/icons-material";
 
-interface BreadcrumbsProps {
-  currentPageName: string;
-}
-
-const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ currentPageName }) => {
-  const pathname = usePathname();
-  const style = useTheme();
-
-  const routeParts = pathname.split("/").filter(Boolean);
-
-  const currentPage = routeParts[routeParts.length - 1];
-
-  const formattedCurrentPage = currentPage
-    ? currentPage.charAt(0).toUpperCase() + currentPage.slice(1).replace(/-/g, " ")
-    : "";
-
-  const lastPage = routeParts[routeParts.length - 2];
-
-  const formattedLastPage = lastPage ? lastPage.charAt(0).toUpperCase() + lastPage.slice(1).replace(/-/g, " ") : "";
-
+const BreadCrumbs: React.FC = () => {
   return (
-    <MyBreadcrumbs
-      separator={<NavigateNextIcon fontSize='small' />}
-      aria-label='breadcrumb'
-      sx={{ "& ol": { flexWrap: "wrap" }, color: style.palette.primary.main, marginBottom: "36px" }}
-    >
-      {formattedLastPage && (
-        <Link color={style.palette.primary.main} href={`/${lastPage}`}>
-          {formattedLastPage}
+    <Box sx={{ mt: 12 }}>
+      <Breadcrumbs separator={<NavigateNext fontSize="small" />}>
+        <Link underline="hover" color="inherit" href="/forum">
+          Forum
         </Link>
-      )}
-      {formattedCurrentPage && <Typography color='text.primary'>{currentPageName}</Typography>}
-    </MyBreadcrumbs>
+        <Link underline="hover" color="inherit" href="">
+          Details
+        </Link>
+      </Breadcrumbs>
+    </Box>
   );
 };
 
-export default Breadcrumbs;
+export default BreadCrumbs;
