31c2e18c49e930b87334c2b7c281ab630a16812f
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2024-04-18 21:50:07 +0700

    feat: fix spacing

--- src/app/api/commentThread/[slug1]/[slug2]/route.ts
+++ src/app/api/commentThread/[slug1]/[slug2]/route.ts
@@ -1,6 +1,5 @@
 import { PostComment } from "@/server-action/controllers/PostComment.controller";
 import { NextRequest } from "next/server";
 export async function POST(req: NextRequest, { params }: any) {
-    return await PostComment(req,Number(params.slug1), Number(params.slug2));
-  }
-  
+    return await PostComment(req, Number(params.slug1), Number(params.slug2));
+}
