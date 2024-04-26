1f9bf38d31be256b4394ceb4391a8a5f561a764c
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2024-04-26 20:58:18 +0700

    Changing Route API

--- src/app/api/postThread/route.ts
+++ src/app/api/postThread/route.ts
@@ -1,5 +1,5 @@
 import { postingThreadController } from "@/server-action/controllers/PostThreads.controller";
 import { NextRequest, NextResponse } from "next/server";
-export async function POST(res: NextResponse, req: NextRequest) {
+export async function POST(req: NextRequest, res: NextResponse) {
     return await postingThreadController(req, res);
 }
