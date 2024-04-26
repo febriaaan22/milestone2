930528453ae7a0f8b906f28a0c4cbfe3fae4f479
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2024-04-26 21:16:20 +0700

    Delete Response

--- src/app/api/postThread/route.ts
+++ src/app/api/postThread/route.ts
@@ -1,5 +1,5 @@
 import { postingThreadController } from "@/server-action/controllers/PostThreads.controller";
-import { NextRequest, NextResponse } from "next/server";
-export async function POST(req: NextRequest, res: NextResponse) {
-    return await postingThreadController(req, res);
+import { NextRequest } from "next/server";
+export async function POST(req: NextRequest) {
+    return await postingThreadController(req);
 }
